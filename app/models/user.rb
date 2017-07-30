9a746210a4f7873a137a7c89df763f1af0235505
shistriver <954917356@qq.com>
2017-07-30 17:53:40 +0800

    add product_list model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  has_many :orders
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
@@ -8,5 +9,4 @@ class User < ApplicationRecord
     is_admin
   end
 
-  has_many :orders
 end
